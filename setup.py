7a8022dc1b8ecdb9fc4626b6b830236351c19656
ntoff <norahtihtzoff@gmail.com>
2017-12-11 08:37:13 +1000

    description + name

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """Shortcut to  mjpeg streamer's control page in the navbar."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "ntoff"
