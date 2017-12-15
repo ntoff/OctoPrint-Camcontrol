a5881824dc95b8db492d92db189b376aa5021a7c
ntoff <norahtihtzoff@gmail.com>
2017-12-15 18:43:33 +1000

    class + title

--- octoprint_camcontrol/static/js/camcontrol.js
+++ octoprint_camcontrol/static/js/camcontrol.js
@@ -12,7 +12,7 @@ $(function() {
         
         $("#navbar_settings").before("\
             <li id=\"#navbar_plugin_camcontrol\">\
-                <a href=\"" + self.control_url() + "\" target=\"_blank\"><span class=\"fa fa-video-camera\"></span></a>\
+                <a class=\"pull-right\" href=\"" + self.control_url() + "\" target=\"_blank\" title=\"mjpeg Control\"><span class=\"fa fa-video-camera\"></span></a>\
             </li>\
         ");
     }
